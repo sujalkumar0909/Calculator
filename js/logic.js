cecaf3d384e8b1552437da812e5becf13ccd883c
Sujal <sujalkumar7209@gmail.com>
2024-06-16 20:01:56 +0530

    modified:   css/style.css 	modified:   index.html 	modified:   js/logic.js

--- js/logic.js
+++ js/logic.js
@@ -1,15 +1,20 @@
 let string="";
-let buttons=document.querySelectorAll(".button");
+let buttons=document.querySelectorAll("button");
+let q=document.querySelector("input");
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click',(e)=>{
-        if(e.target.innerHTML=='='){
+    if(e.target.innerHTML=='=')
+        {
             string=eval(string);
-            document.querySelector('input').value=string;
+            q.value=string;
         }
+        else if(e.target.innerHTML=='C')
+            {
+                q.value=null ;
+            }
         else{
-        string=string+e.target.innerHTML;
-        let q=document.querySelector('input').value=string;
-        q.innerHTML=string;
+            string=string+e.target.innerHTML;
+            q.value=string;
         }
-    })
+})
 })
